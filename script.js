4d7ed29998d94a910e8e2647fb39c7c8e66f24f8
Sid-1819 <shirdhankarsiddhesh@atharvacoe.ac.in>
2024-01-08 09:42:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+console.log("Hello");
+// var a = 1;
+// var b = 9;
+// {
+//   let a = 7; //thala for a reason
+
+//   // let preferred over var
+//   console.log(a + b);
+// }
+// let x = "Hello"; //String
+// let y = 22; // Number
+// let z = true; //Boolean
+// let n = null; //Object******
+// let u = undefined;
+
+// console.log(typeof x, typeof y, typeof z, typeof n, typeof u);
+
+// let o = {
+//   name: "John",
+//   "job role": "Web Developer",
+//   isDeveloper: true,
+// };
+// console.log(o);
+// let a = 8;
+// if (a >= 18) {
+//   console.log("you can drive");
+// } else {
+//   console.log("you cannot drive");
+// }
+
+// function fun(name) {
+//   console.log("Hello " + name + " nice to meet you!");
+//   console.log("Hello " + name + " where are you from!");
+//   console.log("Hello " + name + " are you okay?");
+//   console.log("Hello " + name + " not so good to meet you!");
+// }
+
+// fun("Peter");
+
+// function sum(a, b) {
+//   return a + b;
+// }
+// result1 = sum(11, 8);
+// result2 = sum(10, 9);
+// result3 = sum(12, 7);
+// console.log(result1);
+// console.log(result2);
+// console.log(result3);
+// console.log(result1 + result2 + result3);
